9947f6f4a7ed175fe41d41235ee2347c01c7afa3
Ricky Cheng <rickycheng@hotmail.co.uk>
2019-11-10 19:25:34 +0000

    Added attributes to the migration files

--- database/migrations/2019_11_10_173919_create_posts_table.php
+++ database/migrations/2019_11_10_173919_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('post_body')
             $table->timestamps();
         });
     }
--- database/migrations/2019_11_10_174005_create_comments_table.php
+++ database/migrations/2019_11_10_174005_create_comments_table.php
@@ -15,6 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('comment_body')
             $table->timestamps();
         });
     }
